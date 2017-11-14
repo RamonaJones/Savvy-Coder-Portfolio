0223109ce50057b9abc82c4ea3b7e586a144bb88
Ramona Jones <ramonajonescareer@gmail.com>
2017-11-14 13:41:28 -0600

    Add JavaScript

--- js/greetings.js
+++ js/greetings.js
@@ -1,14 +1,19 @@
 var hello = "hello";
 
 /* eslint-disable no-console */
-var userName = prompt( "what's your name?" );
-var checkUserName = function checkUserName(){
+var originalName = prompt( "what's your name?" );
+var checkUserName = function checkUserName( userName ){
     if( !userName || userName === " " ){
         userName = prompt( "Stop playing and type your real name please." );
-        checkUserName();
+        checkUserName( userName );
+    }
+    else{
+        originalName = userName;
     }
 };
 
-checkUserName();
+checkUserName( originalName );
 
-alert( hello + " " + userName );
+var node = document.querySelector( "h1" ).textConotent = "Hello," + originalName;
+
+alert( node );
